b1a205beeb4096e43e8dfb45e547a1488dd23fda
youHappyOK <1105459187@qq.com>
2016-11-28 16:46:42 +0800

    2016-11-28

---
+++ src/main/java/LightMS/LightMS/remoting/App.java
@@ -0,0 +1,13 @@
+package LightMS.LightMS.remoting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
